4e3118b27b95c6a327c0f712db9ff6e1cc9cdbc6
BiancaBranzei <bianca-gabriela.branzei@student.tuiasi.ro>
2023-07-14 19:45:22 +0300

    Style for header&footer

--- frontend/travel-blog/src/app/app.module.ts
+++ frontend/travel-blog/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -18,9 +21,13 @@ import { FooterComponent } from './common-components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [],
+    HttpClientModule,
+      BrowserAnimationsModule,
+      TuiRootModule,
+      TuiDialogModule,
+      TuiAlertModule
+],
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
