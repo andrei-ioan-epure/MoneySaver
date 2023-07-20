231ea4b63e6b4fed415c83f8c0d69638054f0e52
andrei-ioan-epure <epureandrei567@gmail.com>
2023-07-20 22:02:12 +0300

    Added the filter component and some updates

--- frontend/travel-blog/src/app/blog/blog.module.ts
+++ frontend/travel-blog/src/app/blog/blog.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { BlogRoutingModule } from './blog-routing.module';
 import { ArticleComponent } from './articles/articles.component';
 import { ArticleListItemComponent } from './articles/article-list-item/article-list-item.component';
+import { FilterComponent } from './filter/filter.component';
 
 
 @NgModule({
   declarations: [
     ArticleComponent,
-    ArticleListItemComponent
+    ArticleListItemComponent,
+    FilterComponent
   ],
   imports: [
     CommonModule,
