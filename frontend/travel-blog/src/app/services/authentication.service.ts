494778623aa5ed3731a2cc3327436f8cf2b8396f
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2023-07-26 02:42:11 +0200

    final sign in

--- frontend/travel-blog/src/app/services/authentication.service.ts
+++ frontend/travel-blog/src/app/services/authentication.service.ts
@@ -1,6 +1,7 @@
-// authentication.service.ts
-
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
   private isLogged = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   getIsLogged(): boolean {
     return this.isLogged;
@@ -16,5 +17,25 @@ export class AuthenticationService {
 
   setIsLogged(value: boolean): void {
     this.isLogged = value;
+  }
+
+  login(email: string, password: string): Observable<boolean> {
+    const userLoginDto = { email, password };
+
+    return this.http.post<any>(`https://localhost:7207/api/User/login`, userLoginDto).pipe(
+      map(response => {
+        this.setIsLogged(true);
+        return true;
+      }),
+      catchError(error => {
+        console.error('Login failed:', error);
+        this.setIsLogged(false);
+        return of(false);
+      })
+    );
+  }
+
+  logout(): void {
+    this.setIsLogged(false);
   }
 }
--- frontend/travel-blog/src/app/signin/signin.component.ts
+++ frontend/travel-blog/src/app/signin/signin.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
@@ -18,7 +16,7 @@ export class SigninComponent {
   loginSuccessMessage = '';
   loginErrorMessage = '';
 
-  constructor(private router: Router, private http: HttpClient,private authService: AuthenticationService) {}
+  constructor(private router: Router, private authService: AuthenticationService) {}
 
   ngOnInit() {
     this.formGroup = new FormGroup({
@@ -29,30 +27,25 @@ export class SigninComponent {
 
   submitForm() {
     if (this.formGroup.valid) {
-      const userLoginDto = {
-        email: this.email.value,
-        password: this.password.value
-      };
-
-      this.http.post<any>(`https://localhost:7207/api/User/login`, userLoginDto).subscribe(
-        response => {
-          // Dacă login-ul este cu succes, vei primi un mesaj în răspuns.
-          console.log('Login success');
-          this.authService.setIsLogged(true); // Setează starea de autentificare în serviciul de autentificare
-          this.loginSuccessMessage = 'Logging is successful. You will be redirected to the main page in a few seconds.';
-          this.loginErrorMessage = '';
-          setTimeout(() => {
-            this.router.navigate(['home']); // Redirecționare către pagina de home 
-          }, 5000);
-        },
-        error => {
-          
-          console.error('Login failed:', error);
-          this.authService.setIsLogged(false); // Setează starea de autentificare în serviciul de autentificare
-          this.loginSuccessMessage = '';
-          this.loginErrorMessage = 'Logging error. Please verify your authentication credentials.';
+      this.authService.login(this.email.value, this.password.value).subscribe(
+        success => {
+          if (success) {
+            console.log('Login success');
+            this.loginSuccessMessage = 'Logging is successful. You will be redirected to the main page in a few seconds.';
+            this.loginErrorMessage = '';
+            setTimeout(() => {
+              this.router.navigate(['home']);
+            }, 5000);
+          } else {
+            console.error('Login failed');
+            this.loginSuccessMessage = '';
+            this.loginErrorMessage = 'Logging error. Please verify your authentication credentials.';
+          }
         }
       );
     }
   }
+
+  logout() {
+    this.authService.logout();  }
 }
