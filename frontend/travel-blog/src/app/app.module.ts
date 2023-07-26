125ab5cb93a609f849dd7fdcc49130ea562a0626
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2023-07-26 02:19:17 +0200

    add paginator

--- frontend/travel-blog/src/app/app.module.ts
+++ frontend/travel-blog/src/app/app.module.ts
@@ -21,7 +21,6 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
-import { AuthenticationService } from './services/authentication.service';
 
 
 @NgModule({
--- frontend/travel-blog/src/app/blog/articles/articles.component.ts
+++ frontend/travel-blog/src/app/blog/articles/articles.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Articles } from '../model/article';
 import { ArticlesService } from 'src/app/services/articles.service';
 import { Router } from '@angular/router';
@@ -10,29 +9,57 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-
 export class ArticleComponent implements OnInit {
   articles?: Articles;
-  url?:string;
-  isFavouritesPage?:boolean=false;
-  isNotFavouritesPage?:boolean=true;
+  url?: string;
+  isFavouritesPage?: boolean = false;
+  isNotFavouritesPage?: boolean = true;
+  articlesToShow?: Articles = [];
+  itemsPerPage: number = 6;
+  currentPage: number = 1;
+  totalPages: number = 0; // Declare totalPages variable
 
-  constructor(private readonly articlesService: ArticlesService, private readonly router:Router,private readonly httpService:HttpService) {}
+  constructor(
+    private readonly articlesService: ArticlesService,
+    private readonly router: Router,
+    private readonly httpService: HttpService
+  ) {}
 
   ngOnInit() {
-    //  this.articles = this.articlesService.getArticles();
-        this.httpService.getArticles().subscribe(data=>
-      //console.log(data));
-      this.articles=data); 
-     this.url=this.router.url;
-      if(this.url.includes('favourites')){
-        this.isFavouritesPage=true;
-        this.isNotFavouritesPage=!this.isFavouritesPage;
-      }
-
+    this.httpService.getArticles().subscribe((data) => {
+      this.articles = data;
+      this.totalPages = Math.ceil((this.articles?.length || 0) / this.itemsPerPage); // Actualizează numărul total de pagini
+      this.paginateArticles();
+    });
+  
+    this.url = this.router.url;
+    if (this.url.includes('favourites')) {
+      this.isFavouritesPage = true;
+      this.isNotFavouritesPage = !this.isFavouritesPage;
+    }
   }
+  
   onArticleChange(value: string): void {
     console.log('clicked on', value);
   }
-  
+
+  private paginateArticles() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.articlesToShow = this.articles?.slice(startIndex, endIndex);
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.paginateArticles();
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.paginateArticles();
+    }
+  }
 }
