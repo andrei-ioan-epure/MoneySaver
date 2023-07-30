a4b9ab05a5b2916576a6866b50f686c4db78bc47
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2023-07-30 14:05:56 +0200

    copy button and go to website button

--- frontend/travel-blog/src/app/blog/articles/full-offer/full-offer.component.ts
+++ frontend/travel-blog/src/app/blog/articles/full-offer/full-offer.component.ts
@@ -24,6 +24,7 @@ export class FullOfferComponent implements OnInit {
   content?: string;
   store?: string;
   creatorId?: number;
+  copied = false;
 
   constructor(
     private offerService: OfferService,
@@ -85,4 +86,79 @@ export class FullOfferComponent implements OnInit {
     const sanitizedStoreName = store.toLowerCase();
     return `../../../assets/images/${sanitizedStoreName}.jpg`;
   }
+  goToWebsite() {
+    if (this.store) {
+      // Mapare între numele magazinului și link-ul corespunzător
+      switch (this.store.toLowerCase()) {
+        case 'fashion-days':
+          window.open('https://www.fashiondays.ro/', '_blank');//deschide intr-o fereastra noua
+          break;
+        case 'cupio':
+          window.open('https://www.cupio.ro/', '_blank');
+          break;
+        case 'glovo':
+          window.open('https://glovoapp.com/','_blank');
+          break;
+        case 'answear':
+          window.open('https://answear.ro/','_blank');
+          break;
+        case 'douglas':
+          window.open('https://www.douglas.ro/','_blank');
+          break;
+        case 'netflix':
+          window.open('https://www.netflix.com/ro-en/','_blank');
+          break;
+        case 'spotify':
+          window.open('https://open.spotify.com/','_blank');
+          break;
+        case 'stradivarius':
+          window.open('https://www.stradivarius.com/ro/','_blank');
+          break;
+        case 'tazz':
+          window.open('https://tazz.ro/','_blank');
+          break;
+        // Dacă nu se potrivește cu niciun magazin cunoscut, poți adăuga un comportament alternativ sau un mesaj de eroare.
+        default:
+          console.error("No website link available for this store.");
+          break;
+      }
+    } else {
+      console.error("No store name available for this offer.");
+    }
+  }
+  copyCode() {
+    if (this.code) {
+      try {
+        // Copiază textul în clipboard
+        this.copyToClipboard(this.code);
+        this.copied = true; // Marchează că textul a fost copiat cu succes
+  
+        // Resetează starea "copied" după 3 secunde
+        setTimeout(() => {
+          this.copied = false;
+        }, 3000);
+      } catch (err) {
+        console.error('Copy failed: ', err);
+      }
+    }
+  }
+  
+  copyToClipboard(text: string) {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+    textArea.style.position = 'fixed'; // Asigură-te că nu afectează aspectul paginii
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      document.execCommand('copy');
+    } catch (err) {
+      console.error('Copy failed: ', err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
+  
+ 
 }
