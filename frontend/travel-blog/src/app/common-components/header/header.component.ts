ebd1efa0b3f05d1677c36d797253111604265e51
Mari <axana-marinela.diaconu@student.tuiasi.ro>
2023-08-02 02:14:37 +0200

    add icon for logged user

--- frontend/travel-blog/src/app/common-components/header/header.component.ts
+++ frontend/travel-blog/src/app/common-components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ArticlesService } from 'src/app/services/articles.service';
-import { Observable, Subject, Subscriber } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { NavigationEnd, Router } from '@angular/router';
 
@@ -9,24 +9,26 @@ import { NavigationEnd, Router } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent{
+export class HeaderComponent {
   ImagePath: string;
   public isLoggedIn: Observable<boolean>;
-  public active: string="";
+  public active: string = '';
+  public user: string = ''; // Variabila pentru a stoca initiala utilizatorului logat
+  public showLogout: boolean = false;
 
   constructor(
     private readonly authService: AuthService,
     private articleService: ArticlesService,
-    private readonly router: Router,
+    private readonly router: Router
   ) {
     this.ImagePath = '/assets/images/Logo.png';
     this.isLoggedIn = this.authService.isLoggedIn();
 
-    this.router.events.subscribe(events => {
+    this.router.events.subscribe((events) => {
       if (events instanceof NavigationEnd) {
         this.active = this.router.url;
       }
-    }); 
+    });
   }
 
   logOut(): void {
@@ -38,5 +40,20 @@ export class HeaderComponent{
     this.articleService.setSearchTerm(searchTerm);
   }
 
-  
+  // Metoda pentru a obține inițiala din username ul utilizatorului
+  getInitials(): string {
+    const userEmail = this.authService.getUserInitials();
+    if (!userEmail) return '';
+
+    const username = userEmail.trim().split('@')[0];
+    const nameInitials = username
+      .split('.')
+      .map((namePart) => namePart.charAt(0).toUpperCase())
+      .join('');
+
+    return nameInitials;
+  }
+  onClickIcon(): void {
+    this.showLogout = !this.showLogout;
+  }
 }
