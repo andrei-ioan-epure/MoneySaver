a3c9c2e80427cf2132e9eafad1bb82da489d60a1
Miron Sebastian <sebi147852@yahoo.ro>
2023-08-02 22:02:56 +0300

    Added guards

--- frontend/travel-blog/src/app/blog/blog-routing.module.ts
+++ frontend/travel-blog/src/app/blog/blog-routing.module.ts
@@ -4,6 +4,7 @@ import { ArticleComponent } from './articles/articles.component';
 import { FullOfferComponent } from './articles/full-offer/full-offer.component';
 import { authGuard } from '../guards/auth.guard';
 import { EditOfferComponent } from './articles/full-offer/edit-offer/edit-offer.component';
+import { adminGuard } from '../guards/admin.guard';
 
 const routes: Routes = [
   {
@@ -21,7 +22,8 @@ const routes: Routes = [
  },
  {
   path:'article-list/full-offer/:id/edit-offer',
-  component:EditOfferComponent
+  component:EditOfferComponent,
+  canActivate:[adminGuard]
  },
   {
     path: '**',
--- frontend/travel-blog/src/app/guards/admin.guard.ts
+++ frontend/travel-blog/src/app/guards/admin.guard.ts
@@ -2,13 +2,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 
-export const roleGuard: CanActivateFn = (route, state) => {
+export const adminGuard: CanActivateFn = (route, state) => {
   const authService: AuthService = inject(AuthService);
   const router : Router = inject(Router);
 
   if(authService.getRole() != "admin")
   {
-    router.navigate(['/home']);
+    router.navigate(['/signin']);
     return false;
   }
   return true;
